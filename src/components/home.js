4e0f5769a9a45bfe1fabdb8d78302d57b073fcc7
Hapachino <erickwbrownfield@gmail.com>
2019-01-23 12:21:41 -0800

    completed project setup

---
+++ src/components/home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default props => (
+  <div className="center">
+    <h1>Welcome to chatty App</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero magnam asperiores quo dolorem nisi iure blanditiis dolor unde aperiam odio?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, consequuntur?</p>
+  </div>
+);
